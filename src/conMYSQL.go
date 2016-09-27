943ee72ef84e52d5820b7c0fa5e8fd6dca53fa53
hnguyen <hung135@hotmail.com>
2016-09-27 10:59:39 -0400

    adding more params

--- src/conMYSQL.go
+++ src/conMYSQL.go
@@ -13,15 +13,17 @@ import (
 func main() {
 
 	var mysqlDumpPath string
-
+	var TargetPath string
 	f := configreader.ReadConfig("../src/dbconn.json")
 	if runtime.GOOS == "windows" {
 		fmt.Println("Hello from Windows")
 		fmt.Print(f.Object.Executables[1].Mysqldump)
 		mysqlDumpPath =f.Object.Executables[1].Mysqldump
+		TargetPath=f.Object.Executables[1].TargetDir
 	} else {fmt.Println("Hello from Linux")
 		fmt.Print(f.Object.Executables[0].Mysqldump)
 		mysqlDumpPath =f.Object.Executables[0].Mysqldump
+		TargetPath=f.Object.Executables[0].TargetDir
 	}
 
 
@@ -29,7 +31,7 @@ func main() {
 	cmdName := configreader.SetMysqlExe(mysqlDumpPath)
 	//fmt.Print(configreader.SetMysqlExe("mysqldump.exe"))
 
-	mysqldumper.DumpFile(f,cmdName)
+	mysqldumper.DumpFile(f,cmdName,TargetPath)
 
 
 
