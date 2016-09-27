1fa55de678eaec12f2c729e7e846bb33781e5dc4
hnguyen <hung135@hotmail.com>
2016-09-27 16:17:26 -0400

    adding more params

--- src/conMYSQL.go
+++ src/conMYSQL.go
@@ -10,42 +10,34 @@ import (
 	"os"
 )
 
-
 func main() {
-
-
-
 
 	var mysqlDumpPath string
 	var TargetPath string
 
 	argsWithProg := os.Args
 	var jsonPath string
-	if (len(argsWithProg)==1){
-		jsonPath="../src/dbconn.json"
-	} else{
-		jsonPath=os.Args[1]
+	if (len(argsWithProg) == 1) {
+		jsonPath = "./dbconn.json"
+	} else {
+		jsonPath = os.Args[1]
 	}
 
 	f := configreader.ReadConfig(jsonPath)
 	if runtime.GOOS == "windows" {
 
 		fmt.Print(f.Object.Executables[1].Mysqldump)
-		mysqlDumpPath =f.Object.Executables[1].Mysqldump
-		TargetPath=f.Object.Executables[1].TargetDir
+		mysqlDumpPath = f.Object.Executables[1].Mysqldump
+		TargetPath = f.Object.Executables[1].TargetDir
 	} else {
 		fmt.Print(f.Object.Executables[0].Mysqldump)
-		mysqlDumpPath =f.Object.Executables[0].Mysqldump
-		TargetPath=f.Object.Executables[0].TargetDir
+		mysqlDumpPath = f.Object.Executables[0].Mysqldump
+		TargetPath = f.Object.Executables[0].TargetDir
 	}
-
-
 
 	cmdName := configreader.SetMysqlExe(mysqlDumpPath)
 	//fmt.Print(configreader.SetMysqlExe("mysqldump.exe"))
 
-	mysqldumper.DumpFile(f,cmdName,TargetPath)
-
-
+	mysqldumper.DumpFile(f, cmdName, TargetPath)
 
 }
